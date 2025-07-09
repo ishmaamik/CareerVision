7abc2de5b5a7f5cd24e649c5774b56959f12c402
Ishmaam Iftekhar Khan <ishmaam@iut-dhaka.edu>
2025-07-09 21:45:22 +0600

    Server initialized

---
+++ server/src/main/java/CareerVision/CareerVisionApplication.java
@@ -0,0 +1,11 @@
+package CareerVision;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareerVisionApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(CareerVisionApplication.class, args);
+	}
+}
---
+++ server/src/test/java/CareerVision/CareerVisionApplicationTests.java
@@ -0,0 +1,13 @@
+package CareerVision;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareerVisionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
