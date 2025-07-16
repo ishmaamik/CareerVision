3827c98b2af8e180b676c38cbe2d8bda41b22dfd
Siyam <bhuiyansiyam294@gmail.com>
2025-07-17 03:12:00 +0600

    user crud with postgre support

--- server/src/main/java/CareerVision/Repos/UserRepo.java
+++
@@ -1,7 +0,0 @@
-package CareerVision.Repos;
-
-import CareerVision.Models.Users;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepo extends JpaRepository<Users, Long> {
-}
---
+++ server/src/main/java/CareerVision/controller/UserController.java
@@ -0,0 +1,34 @@
+package CareerVision.controller;
+
+import CareerVision.model.User;
+import CareerVision.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@RestController
+@RequestMapping("/api/users")
+@CrossOrigin(origins = "*")
+public class UserController {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
+
+    @Autowired
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = new BCryptPasswordEncoder();
+    }
+
+    // 🟡 User Registration
+    @PostMapping("/signup")
+    public String signup(@RequestBody User user) {
+        if (userRepository.findByEmail(user.getEmail()).isPresent()) {
+            return "Error: Email already in use";
+        }
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setRole("user"); // default role
+        userRepository.save(user);
+        return "Signup successful";
+    }
+}
