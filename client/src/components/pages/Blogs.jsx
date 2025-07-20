1a00f0105d259650a0b874002132c39b7cee5c8d
Ishmaam Iftekhar Khan <ishmaam@iut-dhaka.edu>
2025-07-21 00:50:48 +0600

    Profile done

--- client/src/components/pages/Blogs.jsx
+++ client/src/components/pages/Blogs.jsx
@@ -27,75 +27,75 @@ const Blogs = () => {
         setExpandedIndex(prevState => prevState.includes(index) ? prevState.filter(expand => expand != index) : [...prevState, index]); // Toggle expand/collapse for each blog
     };
 
-    
+
 
 
 
     return (
         <>
-            <div className={`flex mt-20 ${loading? 'ml-24' :'ml-99'}`}>
+            <div className={`flex mt-20 ${loading ? 'ml-24' : 'ml-99'}`}>
                 <Button disableRipple variant="text" sx={{ height: '4px', color: 'gray', ":hover": { backgroundColor: 'transparent', color: 'indigo' }, ":focus-visible": { outline: 'none' } }} onClick={() => navigate('/blogs/newBlog')} >
                     Create New Blog
                 </Button>
             </div>
 
-            {loading ? 
-                 <div className=" items-center mt-24 ml-10">
-                 <div className="w-full max-w-2xl rounded-lg bg-white py-6 px-20 shadow-lg">
-                     <p>Loading Blogs...</p>
-                 </div>
-             </div>
-            :
-            <div className='flex ml-20'>
-                <div className="flex flex-wrap gap-4 items-start">
-                    {
-                        blogs.length > 0 ? blogs.map((p, index) => {
-
-                            const previewContent = p.content.substring(0, 100); // Limit content to 200 chars for preview
-                            const isContentLong = p.content.length > 100;
-                            const showFullContent = expandedIndex.includes(index);
-
-                            return (
-                                <>
-                                    <div key={index} className=" flex ml-3 mt-12">
-                                        <div className={`break-words ${showFullContent ? 'h-auto' : 'h-40'} w-60  rounded-lg bg-white p-4 shadow-lg`}>
-                                            <b className='text-xs justify-center'>{p.title}</b>
-
-                                            {/*Content*/}
-
-                                            <div className="mt-2">
-                                                <p style={{ fontSize: '11px' }}>{showFullContent ? p.content : `${previewContent}...`}</p>
-                                            </div>
-                                            {isContentLong && (
-                                                <Button disableRipple variant="text" sx={{ height: '4px', fontSize: '11px', color: 'gray', ":hover": { backgroundColor: 'transparent' }, ":focus-visible": { outline: 'none' } }} onClick={() => handleToggleContent(index)} >
-
-                                                    {showFullContent ? 'See Less' : 'See More'}
-                                                </Button>
-                                            )}
-
-                                            <i className='text-xs block mt-2 pr-1'>~{p.author}</i>
-                                        </div>
-                                    </div>
-                                </>
-                            );
-                        })
-
-                            :
-
-                            <div className="fixed  left-1/2 transform -translate-x-1/2  flex items-center justify-center min-h-screen">
-                                <div className="w-full max-w-2xl rounded-lg bg-white py-6 px-20 shadow-lg">
-                                    <p>No Blogs Currently Available</p>
-                                </div>
-                            </div>
-                    }
-
-
+            {loading ?
+                <div className=" items-center mt-24 ml-10">
+                    <div className="w-full max-w-2xl rounded-lg bg-white py-6 px-20 shadow-lg">
+                        <p>Loading Blogs...</p>
+                    </div>
                 </div>
+                :
+                <div className='flex ml-20'>
+                    <div className="flex flex-wrap gap-4 items-start">
+                        {
+                            blogs.length > 0 ? blogs.map((p, index) => {
+
+                                const previewContent = p.content.substring(0, 100); // Limit content to 200 chars for preview
+                                const isContentLong = p.content.length > 100;
+                                const showFullContent = expandedIndex.includes(index);
+
+                                return (
+                                    <>
+                                        <div key={index} className=" flex ml-3 mt-12">
+                                            <div className={`break-words ${showFullContent ? 'h-auto' : 'h-40'} w-60  rounded-lg bg-white p-4 shadow-lg`}>
+                                                <b className='text-xs justify-center'>{p.title}</b>
+
+                                                {/*Content*/}
+
+                                                <div className="mt-2">
+                                                    <p style={{ fontSize: '11px' }}>{showFullContent ? p.content : `${previewContent}...`}</p>
+                                                </div>
+                                                {isContentLong && (
+                                                    <Button disableRipple variant="text" sx={{ height: '4px', fontSize: '11px', color: 'gray', ":hover": { backgroundColor: 'transparent' }, ":focus-visible": { outline: 'none' } }} onClick={() => handleToggleContent(index)} >
+
+                                                        {showFullContent ? 'See Less' : 'See More'}
+                                                    </Button>
+                                                )}
+
+                                                <i className='text-xs block mt-2 pr-1'>~{p.author}</i>
+                                            </div>
+                                        </div>
+                                    </>
+                                );
+                            })
+
+                                :
+
+                                <div className="fixed  left-1/2 transform -translate-x-1/2  flex items-center justify-center min-h-screen">
+                                    <div className="w-full max-w-2xl rounded-lg bg-white py-6 px-20 shadow-lg">
+                                        <p>No Blogs Currently Available</p>
+                                    </div>
+                                </div>
+                        }
 
 
-            </div >}
+                    </div>
 
-            
+
+                </div >}
+
+
         </>
     )
 }
---
+++ client/src/components/pages/Profile.jsx
@@ -0,0 +1,95 @@
+import React, { useContext } from 'react'
+import ishu from '../../assets/siyam.jpg'
+import { User } from '../../context/UserContext'
+const Profile = () => {
+
+    const { userDetails } = useContext(User)
+    return (
+        <>
+            <div className='flex'>
+                <div>
+                    <div className="relative top-30 right-20">
+                        <div className="relative w-120 h-auto max-h-60  rounded-lg  bg-white shadow-lg">
+
+                            <div className='relative bg-gray-100 flex w-full h-20 whitespace-nowrap rounded-lg' >
+                                <p className="font-semibold absolute left-4 top-2">Your Profile</p>
+                                <p className="   whitespace-nowrap absolute right-4">Joined 20/07/25</p>
+
+                            </div>
+
+
+                            <div className='relative bg-white h-50 rounded-lg'>
+                                <img className='absolute left-2 top-5 w-16 ' style={{ borderRadius: '50%' }} src={ishu} />
+                                <p className='absolute left-2 top-24  '>Siyam Bhuiyan</p>
+                                <p className='absolute left-2 top-32'>01696969420</p>
+                            </div>
+
+                        </div>
+                    </div>
+
+                    <div className="relative top-50 right-20">
+                        <div className="relative w-120 h-auto max-h-60  rounded-lg  bg-white shadow-lg">
+
+                            <div className='relative bg-gray-100 flex w-full h-16 whitespace-nowrap rounded-lg' >
+                                <p className="font-semibold absolute left-4 top-2">Email Address</p>
+                            </div>
+
+
+                            <div className='relative bg-white h-35 rounded-lg whitespace-nowrap'>
+                                <p className='absolute left-2 top-5 bg-blue-100 rounded-2xl text-blue-900 w-30'>Primary Mail</p>
+                                <p className='absolute left-2 top-15  '>siyam@iut-dhaka.edu</p>
+                                <p className='absolute left-2 top-23'>siyambhuiyan@gmail.com</p>
+                            </div>
+
+                        </div>
+                    </div>
+
+                </div>
+                <div>
+                    <div>
+
+                    <div className="relative top-30 left-20">
+                            <div className="relative w-120 h-auto max-h-60  rounded-lg  bg-white shadow-lg">
+
+                                <div className='relative bg-gray-100 flex w-full h-16 whitespace-nowrap rounded-lg' >
+                                    <p className="font-semibold absolute left-4 top-1">Phone Number</p>
+                                </div>
+
+
+                                <div className='relative bg-white h-35 rounded-lg whitespace-nowrap'>
+                                    <p className='absolute left-2 top-5 bg-blue-100 rounded-2xl text-blue-900 w-30'>Primary Number</p>
+                                    <p className='absolute left-2 top-15  '>01696969420</p>
+                                    <p className='absolute left-2 top-23'>01717171717</p>
+                                </div>
+
+                            </div>
+                        </div>
+
+                        <div className="relative top-50 left-20">
+                            <div className="relative w-120 h-auto max-h-60  rounded-lg  bg-white shadow-lg">
+
+                                <div className='relative bg-gray-100 flex w-full h-20 whitespace-nowrap rounded-lg' >
+                                    <p className="font-semibold absolute left-4 top-2">Address</p>
+
+                                </div>
+
+
+                                <div className='relative bg-white h-35 rounded-lg'>
+                                <p className='absolute left-2 top-5 bg-blue-100 rounded-2xl text-blue-900 w-30'>Primary Address</p>
+                                    <p className='absolute left-2 top-15  '>House 14, Road 6, Dhanmondi, Dhaka-1205</p>
+                                    <p className='absolute left-2 top-23'>IUT Male Residence, Board Bazar, Gazipur</p>
+                                </div>
+
+                            </div>
+                        </div>
+
+                        
+
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Profile;
